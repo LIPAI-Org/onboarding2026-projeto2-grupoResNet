dfd5dd4ad82fdbeec41690abe2b6b467d24dbba8
joaogpizza <joao.piza@ufu.br>
2026-05-19 21:02:03 -0300

    Adicao dos criterions bases na configs_base.py

--- configs/configs_base.py
+++ configs/configs_base.py
@@ -5,3 +5,6 @@ BATCH_SIZE = 32
 NUM_EPOCAS = 50
 TAXA_APRENDIZADO = 1e-3
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+OTIMIZADOR = torch.optim.Adam
+CRITERION_MULTI = torch.nn.CrossEntropyLoss()
+CRITERION_BIN = torch.nn.BCEWithLogitsLoss()
