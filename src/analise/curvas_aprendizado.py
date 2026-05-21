41177dceb2173a6d44c50abf9d7ac8914765f7d1
joaogpizza <joao.piza@ufu.br>
2026-05-20 21:06:42 -0300

    alteracoes necessarias para plotar e salvar graficos de loss e acc

--- src/analise/curvas_aprendizado.py
+++ src/analise/curvas_aprendizado.py
@@ -1,1 +1,88 @@
 """ Plota custo e acurácia de treino e validação """
+
+import matplotlib.pyplot as plt
+import os
+import src.utils.paths as paths
+from configs.configs_base import NUM_EPOCAS
+
+def plotar_e_salvar_loss(
+        seed: str,
+        nome_modelo: str,
+        modo_treinamento: str,
+        aumento: str,
+        dataset: str,
+        history: dict[str, list],
+    ):
+    """
+    Plota e salva a perda do treino e da validação para
+    o diretório apontado por paths.PATH_PLOTS.
+    Nota: history vem da função de treino em treinador.py, definida
+    assim:
+    history = {
+        "train_loss": [],
+        "val_loss": [],
+        "train_acc": [],
+        "val_acc": []
+    }
+    """
+    
+    if not os.path.isdir(paths.PATH_PLOTS):
+        os.makedirs(paths.PATH_PLOTS, exist_ok=True)
+    
+    nome_arq = f"loss_{seed}_{nome_modelo}_{modo_treinamento}_{aumento}_{dataset}.png"
+    caminho_saida = os.path.join(paths.PATH_PLOTS, nome_arq)
+
+    epocas = range(1, NUM_EPOCAS + 1)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(epocas, history["train_loss"], label="Train Loss")
+    plt.plot(epocas, history["val_loss"], label="Val Loss")
+
+    plt.title("Loss de Treino e Validação")
+    plt.xlabel("Época")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+    plt.tight_layout()
+
+    plt.savefig(caminho_saida, dpi=300, bbox_inches="tight")
+    plt.close()
+
+    return caminho_saida
+
+def plotar_e_salvar_acc(
+        seed: str,
+        nome_modelo: str,
+        modo_treinamento: str,
+        aumento: str,
+        dataset: str,
+        history: dict[str, list],
+    ):
+    """
+    Plota e salva a acuracia do treino e da validação para
+    o diretório apontado por paths.PATH_PLOTS.
+    """
+    
+    if not os.path.isdir(paths.PATH_PLOTS):
+        os.makedirs(paths.PATH_PLOTS, exist_ok=True)
+    
+    nome_arq = f"acc_{seed}_{nome_modelo}_{modo_treinamento}_{aumento}_{dataset}.png"
+    caminho_saida = os.path.join(paths.PATH_PLOTS, nome_arq)
+
+    epocas = range(1, NUM_EPOCAS + 1)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(epocas, history["train_acc"], label="Acuracia Treino")
+    plt.plot(epocas, history["val_acc"], label="Acuracia Validacao")
+
+    plt.title("Acuracia de Treino e Validação")
+    plt.xlabel("Época")
+    plt.ylabel("Acc")
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+    plt.tight_layout()
+
+    plt.savefig(caminho_saida, dpi=300, bbox_inches="tight")
+    plt.close()
+
+    return caminho_saida
