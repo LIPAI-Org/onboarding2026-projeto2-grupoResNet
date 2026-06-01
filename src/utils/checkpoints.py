b007046eee8a230cb5db42b37b2721f731d7b45c
joaogpizza <joao.piza@ufu.br>
2026-06-01 17:57:22 -0300

    adicionando a logica de checkpoints basica

---
+++ src/utils/checkpoints.py
@@ -0,0 +1,30 @@
+# checkpoints.py
+from pathlib import Path
+import torch
+from configs.configs_base import DEVICE
+
+def save_checkpoint(path, modelo, otim, epoca, best_val_acc, extra=None):
+    path = Path(path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    checkpoint = {
+        "epoca": epoca,
+        "best_val_acc": best_val_acc,
+        "modelo_state_dict": modelo.state_dict(),
+        "otim_state_dict": otim.state_dict(),
+    }
+
+    if extra is not None:
+        checkpoint["extra"] = extra
+
+    torch.save(checkpoint, path)
+
+
+def load_checkpoint(path, modelo, otim=None):
+    checkpoint = torch.load(path, map_location=DEVICE, weights_only=False)
+    modelo.load_state_dict(checkpoint["modelo_state_dict"])
+
+    if otim is not None and "otim_state_dict" in checkpoint:
+        otim.load_state_dict(checkpoint["otim_state_dict"])
+
+    return checkpoint
